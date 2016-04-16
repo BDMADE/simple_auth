e20953afe958435af1800599119e8e66378ae33a
hmtanbir <tanbir2025@gmail.com>
2016-04-16 13:05:58 +0600

    Session Controller created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+   user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url, notice: 'Logged in!'
+    else
+      redirect_to new_session_path,notice: 'Authentication failed !'
+    end
   end
 
   def destroy
+  	session[:user_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
   end
 end
