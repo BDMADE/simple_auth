abf9db367441b05c7f44ee49008ba59bab3ed6cd
hmtanbir <tanbir2025@gmail.com>
2016-04-16 13:17:14 +0600

    Helper method :curent_user created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+# We need a mechanism to access the current_user everywhere in the application. 
+# For that we add a current_user method to the ApplicationController and use helper_method 
+# to provide a helper method for the views too.
   private
   def current_user
     User.where(id: session[:user_id]).first
   end
+  # use this method in everywhere.
   helper_method :current_user
 end
