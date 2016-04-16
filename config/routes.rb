82bec6f39d821c00361d08698431631b8f98b70c
hmtanbir <tanbir2025@gmail.com>
2016-04-16 12:35:19 +0600

    Create home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  resources :users
+  #get 'home/index'
+   root 'home#index'
 
-  get 'home/admin'
+  get 'admin',to:'home#admin'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
