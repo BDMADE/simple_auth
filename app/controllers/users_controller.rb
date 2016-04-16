746f5407d09fab27180b1b6224a7e1630f034fd5
hmtanbir <tanbir2025@gmail.com>
2016-04-16 13:38:36 +0600

    Auto login after creating user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { session[:user_id] = @user.id
+                      redirect_to root_url, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
