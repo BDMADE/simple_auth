24445d018cdf4c3bf3d328d63ca5c549973f0034
hmtanbir <tanbir2025@gmail.com>
2016-04-16 12:56:41 +0600

    Login form created

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
   #get 'home/index'
    root 'home#index'
 
